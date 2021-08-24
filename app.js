3f0a3bbfe6a8569cd96812c8947c202f87525f76
Gabriel Ataide <gabriel.ataide07@gmail.com>
2021-08-24 16:34:25 -0300

    fetching images with javascript

---
+++ app.js
@@ -0,0 +1,25 @@
+const auth = "563492ad6f917000010000019e5b0e5ab68647f5b57e29ffa280326f";
+const gallery = document.querySelector(".gallery");
+const searchInput = document.querySelector('.search-input');
+const submitButton = document.querySelector('submit-btn');
+let searchValue;
+
+async function curatedPhotos(){
+    const dataFetch = await fetch('https://api.pexels.com/v1/curated?per_page=15', {
+        method: 'GET',
+        headers: {
+            Accept: 'application/json',
+            Authorization: auth
+        }
+    });
+    const data = await dataFetch.json();
+    data.photos.forEach(photo => {
+        const galleryImg = document.createElement('div');
+        galleryImg.classList.add('gallery-img');
+        galleryImg.innerHTML = `<img src=${photo.src.large}</img> <p>${photo.photographer}</p>`;
+        gallery.appendChild(galleryImg);
+    });
+}
+
+curatedPhotos();
+
