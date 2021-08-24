9c47fc15d52071d441f7dacaa3954630fd1c3fec
Gabriel Ataide <gabriel.ataide07@gmail.com>
2021-08-24 18:13:57 -0300

    creating the download button

--- app.js
+++ app.js
@@ -11,8 +11,7 @@ form.addEventListener('submit', e => {
     searchPhotos(searchValue);
 });
 
-
-
+//functions
 function updateInput(e){
     searchValue = e.target.value;
 }
@@ -31,7 +30,12 @@ function generatePictures(data){
     data.photos.forEach(photo => {
         const galleryImg = document.createElement('div');
         galleryImg.classList.add('gallery-img');
-        galleryImg.innerHTML = `<img src=${photo.src.large}</img> <p>${photo.photographer}</p>`;
+        galleryImg.innerHTML = `
+         <div  class="gallery-info">
+         <p>${photo.photographer}</p> 
+         <a href=${photo.src.original}>Download</a>
+         </div>
+         <img src=${photo.src.large}</img>`;
         gallery.appendChild(galleryImg);
     });
 }
@@ -40,9 +44,15 @@ async function curatedPhotos(){
     generatePictures(data);
 }
 async function searchPhotos(query){
+    clear();
     const data = await fetchApi(`https://api.pexels.com/v1/search?query=${query}+query&per_page=15&page=1`);
     generatePictures(data);
 }
+function clear(){
+    gallery.innerHTML = '';
+    searchInput.value = '';
+}
+
 
 curatedPhotos();
 
